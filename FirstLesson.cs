4117d9c3e38cac2a4efd48b2d88f69ff96a76cf5
istchanar <istchanar@outlook.com>
2022-09-24 03:57:11 +0700

    Add project files.

---
+++ FirstLesson.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace IntroductionToProgramming
+{
+    enum TaskNumber : int { 
+        SECOND = 2,
+        FOURTH = 4,
+        SIXTH = 6,
+        EIGHTH = 8
+    }
+    internal class FirstLesson
+    {
+        private float firstNumber, secondNumber, thirdNumber, answer;
+        private int taskNumber;
+
+        public void TaskInit()
+        {
+            bool isExpectedNumber;
+            int taskNumber;
+            Console.ForegroundColor = ConsoleColor.Magenta;
+
+            do
+            {
+                Console.Write("Введите номер задания: ");
+                isExpectedNumber = int.TryParse(Console.ReadLine()!, out int result);
+                taskNumber = result;
+            }
+            while ((isExpectedNumber != true) || !(taskNumber is (int)TaskNumber.SECOND or (int)TaskNumber.FOURTH or (int)TaskNumber.SIXTH or (int)TaskNumber.EIGHTH));
+
+            this.taskNumber = taskNumber;
+
+            switch(taskNumber)
+            { 
+                case (int)TaskNumber.SECOND:
+                    SecondTask();
+                    break;
+                case (int)TaskNumber.FOURTH:
+                    FourthTask();
+                    break;
+                case (int)TaskNumber.SIXTH:
+                    SixthTask();
+                    break;
+                default:
+                    EighthTask();
+                    break;
+            }
+        }
+        public void TaskDataSet()
+        {
+
+            try
+            {
+                firstNumber = float.Parse(Console.ReadLine()!);
+
+                if (this.taskNumber == (int)TaskNumber.SECOND || this.taskNumber == (int)TaskNumber.FOURTH)
+                {
+                    secondNumber = float.Parse(Console.ReadLine()!);
+                }
+
+                if (this.taskNumber == (int)TaskNumber.FOURTH)
+                {
+                    thirdNumber = float.Parse(Console.ReadLine()!);
+                }
+
+            }
+            catch (FormatException error)
+            {
+                Console.WriteLine($"Введены неправильные данные: \n {error}");
+            }
+        }
+        public void SecondTask() {
+            Console.WriteLine($"Задача #2 Введите два числа, чтобы узнать большее: ");
+            TaskDataSet();
+
+            if (firstNumber > secondNumber)
+            {
+                Console.WriteLine($"Большее число: {firstNumber}, меньшее число {secondNumber}");
+            }
+            else
+            {
+                Console.WriteLine($"Большее число: {secondNumber}, меньшее число {firstNumber}");
+            }
+        }
+        public void FourthTask()
+        {
+            Console.WriteLine($"Задача #4 Введите три числа, чтобы узнать большее: ");
+            TaskDataSet();
+            answer = firstNumber > secondNumber && firstNumber > thirdNumber
+                   ? firstNumber : (secondNumber > thirdNumber ? secondNumber : thirdNumber);
+            Console.WriteLine($"Большее число: {answer}");
+        }
+        public void SixthTask()
+        {
+            Console.WriteLine($"Задача #6 Для проверки чётности введите число: ");
+            TaskDataSet();
+            Console.WriteLine(firstNumber % 2 == 0 ? $"Число {firstNumber} чётное." : $"Число {firstNumber} нечётное.");
+        }
+        public void EighthTask()
+        {
+
+            Console.WriteLine($"Задача #8 Для показа рада числе от 1 до N c шагом 2 введите число: ");
+            TaskDataSet();
+            answer = firstNumber % 2 == 0 ? firstNumber : firstNumber - 1;
+            Console.WriteLine($"Ряд чисел с шагом в 2: ");
+            do
+            {
+                Console.Write($"{answer} ");
+                answer -= 2;
+            }
+            while (answer > 1);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IntroductionToProgramming;
+using System;
+
+
+var lessonOne = new FirstLesson();
+
+Console.Write("Введите номер урока: ");
+int lessonNumber = Int32.Parse(Console.ReadLine()!);
+
+string exitValue;
+
+do
+{
+    lessonOne.TaskInit();
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+
+    Console.WriteLine("\nДля выхода набрать exit, продолжить - нажать Enter");
+    exitValue = Console.ReadLine()!;
+}
+while (exitValue != "exit");
+
+
+/*
+Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+
+Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+
+Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+
+Console.WriteLine("Задача #10 Введите трёхзначное число для получения цифры во втором разряде: ");
+
+int numberScope;
+bool lengthСheck;
+
+
+do {
+    Console.WriteLine("Введите трёхзначное число: ");
+    lengthСheck = int.TryParse(Console.ReadLine()!, out int number);
+    numberScope = number;
+}
+while (numberScope < 100 || numberScope > 999 || lengthСheck != true);
+Console.WriteLine($"Второе число в трёхзначной цифре: {(numberScope / 10) % 10}");
+
+
+Console.WriteLine("Задача #13 Введите число для получения цифры в третьем разряде: ");
+
+do
+{
+    Console.WriteLine("Введите число: ");
+    lengthСheck = int.TryParse(Console.ReadLine()!, out int number);
+    numberScope = number;
+    if (numberScope > 100) {
+        Console.WriteLine($"Третье число в цифре: {(numberScope / 100) % 10}");
+    }
+    else if(numberScope < 100 && numberScope > -100)
+    {
+        Console.WriteLine($"Третьего числа нет");
+    }
+}
+while (lengthСheck != true);
+*/
